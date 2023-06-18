c4c1affb195c65f2813a8223be4352b35bdb7572
art-gunika <136194511+art-gunika@users.noreply.github.com>
2023-06-18 16:38:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let count = 0
+let countEl = document.getElementById("count-el")
+ let saveEl = document.getElementById("save-el")
+
+ 
+function increment() {
+     count += 1
+     countEl.textContent = count 
+}
+
+function save() {
+    let container = count + " - "
+    saveEl.textContent += container
+    countEl.textContent = 0
+    count = 0
+}
